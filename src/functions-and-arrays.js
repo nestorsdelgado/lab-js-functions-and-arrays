83d9d6008579909d5eac4adc08fbb74a1d0fa829
Nestor Suarez Delgado <nestorsdelgado@gmail.com>
2025-01-16 12:37:09 +0000

    Lab solution. Néstor

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,12 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+
+function maxOfTwoNumbers(firstNumber, secondNumber) {
+    if (firstNumber > secondNumber){
+        return firstNumber
+    }else {
+        return secondNumber
+    }
+}
 
 
 
@@ -7,7 +14,28 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(array) {
+
+    if(array.length == 0){
+        
+        return null
+    }
+      
+    let masGrande = array[0]
+    for(let i = 0;i < array.length;i++){
+        prueba = array[i].length
+
+        if(prueba > masGrande.length) {
+        
+            masGrande = array[i]
+          
+        }
+    
+    }
+    //console.log(masGrande)
+    return masGrande
+
+}
 
 
 
@@ -15,7 +43,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(array) {
+    let sumatoria = 0
+    for(i = 0;i < array.length;i++){
+        sumatoria = sumatoria + array[i]
+    }
+
+    return sumatoria
+}
 
 
 
@@ -23,7 +58,18 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+
+    if(numbers2.length == 0) {
+
+        return 0
+    }
+
+    sumatoria = sumNumbers(numbers2)
+    media = sumatoria/numbers2.length
+
+    return media
+}
 
 
 
@@ -31,4 +77,19 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(array, word) {
+
+    if(array.length == 0){
+        return null
+    }
+
+    for(i = 0;i < array.length;i++){
+
+        if(array[i] == word){
+            return true
+        }
+        
+        continue
+    }
+    return false
+}
